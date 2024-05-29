b16e204e77a018d0991b35e707d2218aa9c5fce5
Catherine Feng <catfeng@Catherines-MacBook-Pro.local>
2024-05-29 16:59:42 -0700

    Table draft complete, left pane draft complete

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,29 @@ export type DataViewProps = {
   salesData: SalesInfo[];
 };
 
+type LeftPaneProps = {
+  title: string;
+  subtitle: string;
+  image: string;
+  tags: string[];
+}
+
 function App() {
-  const salesData: Array<SalesInfo> = jsonData[0]["sales"];
+  const data = jsonData[0];
+  const salesData: Array<SalesInfo> = data["sales"];
   return (
-    <>
+    <div className="body">
       <Banner />
       <div style={{ display: 'flex' }}>
-        <LeftPane />
+        <LeftPane 
+          title={data["title"]}
+          subtitle={data["subtitle"]}
+          image={data["image"]}
+          tags={data["tags"]}
+        />
         <RightPane salesData={salesData} />
       </div>
-    </>
+    </div>
   );
 }
 
@@ -32,8 +45,14 @@ function Banner() {
   return <div className='banner'><img className='banner-logo' src={stackline_logo} alt="Stackline logo" /></div>;
 }
 
-function LeftPane() {
-  return <h2>Left Pane Placeholder</h2>;
+function LeftPane({title, subtitle, tags, image}: LeftPaneProps) {
+  return (
+    <div className='left-pane'>
+      <img className='product-image' src={image} />
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <div className='tags'>{tags.map((tag) => <div key={tag} className="tag">{tag}</div>)}</div>
+    </div>);
 }
 
 function RightPane({ salesData }: DataViewProps) {
